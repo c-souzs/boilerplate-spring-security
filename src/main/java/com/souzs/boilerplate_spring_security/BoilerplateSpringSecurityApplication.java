f6970b1d9697789b153429de084488925eb8b7ff
Caio Souza <souzsdev@gmail.com>
2025-07-10 08:38:41 -0300

    first commit

---
+++ src/main/java/com/souzs/boilerplate_spring_security/BoilerplateSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.souzs.boilerplate_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souzs/boilerplate_spring_security/BoilerplateSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzs.boilerplate_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
