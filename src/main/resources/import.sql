11911c0d04bbe7d6abbfc7e2f54fb1d3ae94033f
Caio Souza <souzsdev@gmail.com>
2025-08-15 18:51:09 -0300

    feat: adjustment in the import script and enum mapping

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,10 @@
-INSERT INTO tb_role (id, name) VALUES (3, 'ROLE_CLIENT');
-INSERT INTO tb_role (id, name) VALUES (2, 'ROLE_MODERATOR');
-INSERT INTO tb_role (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO tb_role (name) VALUES ('ROLE_ADMIN');
+INSERT INTO tb_role (name) VALUES ('ROLE_MODERATOR');
+INSERT INTO tb_role (name) VALUES ('ROLE_USER');
 
-INSERT INTO tb_user (id, email) VALUES (1, 'admin@example.com');
-INSERT INTO tb_user (id, email) VALUES (2, 'moderator@example.com');
-INSERT INTO tb_user (id, email) VALUES (3, 'client@example.com');
+INSERT INTO tb_user (email, password) VALUES ('admin@example.com', '$2a$10$x10ghvYds9Itx2kEsoiGP.ug2f2NAyimkJOcKUm4yuiyW9kSbKx/G');
+INSERT INTO tb_user (email, password) VALUES ('moderator@example.com', '$2a$10$XcfdodwriZqj2r1azxYd7eLji6F2c7VNrDlPxtJGiyRy9ruURQ3bK');
+INSERT INTO tb_user (email, password) VALUES ('user@example.com', '$2a$10$6mRxj1KvEFCfl.4nFsc/z.NrXaVel0FTqxP/US3cPTuHYczTZi9Zy');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
